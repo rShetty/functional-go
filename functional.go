43f836ba2f4f16dc8471209f96133ce683e4f10d
Rajeev N B <rajeevnb@thoughtworks.com>
2015-07-09 23:35:44 +0530

    Make it work with any type Map

--- functional.go
+++ functional.go
@@ -1,12 +1,15 @@
 package functional
 
-type mapFunc func(int) int
+import "reflect"
+
+type mapFunc func(interface{}) interface{}
 
 // Map takes array and a function
-func Map(f mapFunc, array []int) []int {
-	outputArray := make([]int, len(array))
-	for i, a := range array {
-		outputArray[i] = f(a)
+func Map(array interface{}, fn mapFunc) []interface{} {
+	val := reflect.ValueOf(array)
+	outputArray := make([]interface{}, val.Len())
+	for i := 0; i < val.Len(); i++ {
+		outputArray[i] = fn(val.Index(i).Interface())
 	}
 	return outputArray
 }
--- functional_test.go
+++ functional_test.go
@@ -5,12 +5,12 @@ import "testing"
 func TestMap(t *testing.T) {
 	inputList := []int{1, 2, 3}
 
-	square := func(x int) int {
-		return x * x
+	square := func(x interface{}) interface{} {
+		return x.(int) * x.(int)
 	}
 
 	expected := []int{1, 4, 9}
-	actual := Map(square, inputList)
+	actual := Map(inputList, square)
 
 	for i, e := range expected {
 		if e != actual[i] {
